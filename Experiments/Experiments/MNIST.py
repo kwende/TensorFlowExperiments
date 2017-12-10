f6091eb738228f1a76b0361545b869dd2fdb8d35
Ben <ben@ben-rush.net>
2017-12-10 16:46:28 -0600

    Starting to get it to work on MNIST.

--- Experiments/Experiments/MNIST.py
+++ Experiments/Experiments/MNIST.py
@@ -1,5 +1,9 @@
 import tensorflow as tf
 import math
+import numpy as np
+
+from tensorflow.examples.tutorials.mnist import input_data
+from tensorflow.examples.tutorials.mnist import mnist
 
 IMAGE_DIM = 28
 IMAGE_SIZE = IMAGE_DIM * IMAGE_DIM
@@ -7,13 +11,15 @@ IMAGE_SIZE = IMAGE_DIM * IMAGE_DIM
 hidden1_size = 100
 hidden2_size = 30
 output_size = 10
+batch_size = 1
 
-imagePixels = tf.zeros([1, IMAGE_SIZE])
+imagePixelsPlaceholder = tf.placeholder(dtype=tf.float32, shape=[batch_size, 784])
+labelsPlaceholder = tf.placeholder(dtype=tf.int32, shape=[batch_size, 10])
 
 # input layer to first hidden.
 hidden1Weights = tf.Variable(tf.truncated_normal([IMAGE_SIZE, hidden1_size], stddev=1.0 / math.sqrt(IMAGE_SIZE)))
 hidden1Biases = tf.Variable(tf.zeros(hidden1_size))
-hidden1 = tf.nn.relu(tf.matmul(imagePixels, hidden1Weights) + hidden1Biases)
+hidden1 = tf.nn.relu(tf.matmul(imagePixelsPlaceholder, hidden1Weights) + hidden1Biases)
 
 # first hidden to second hidden.
 hidden2Weights = tf.Variable(tf.truncated_normal([hidden1_size, hidden2_size], stddev=1.0 / math.sqrt(hidden1_size)))
@@ -29,4 +35,14 @@ init = tf.global_variables_initializer()
 
 session = tf.Session()
 session.run(init)
+
+mnistData = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+pixels = np.reshape(mnistData.train.images[0], [1, 784])
+labels =  [mnistData.train.labels[0]]
+
+print(session.run(output, {imagePixelsPlaceholder : pixels,
+                     labelsPlaceholder : labels}))
+
+print()
 
